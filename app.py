6c4f4db62bfdd5f7e936a6a462ef6857d1c9711c
Your Name <nong8033@gmail.com>
2024-07-04 21:41:41 +0700

    add bar chart

--- app.py
+++ app.py
@@ -1,34 +1,76 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
 import pandas as pd
-
-df = pd.read_csv('D:\project_eco\stdall_th.csv')
-geojson = px.data.election_geojson()
+import plotly.express as px
+import json
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
 
 app = Dash(__name__)
 
+# Load data
+df = pd.read_csv("D:\project_eco\stdall_th.csv")
+geojson = px.data.election_geojson()
+
+df = df.groupby(['schools_province'])[['totalstd', 'totalmale', 'totalfemale']].mean().reset_index()
+print(df[:5])
+
+# App
 app.layout = html.Div([
-    html.H1(children='Graduation in Thailand', style={'textAlign':'center'}),
-    dcc.Dropdown(df.schools_province.unique(), 'กรุงเทพ', id='dropdown-selection'),
+    html.H1("School Statistics in Thailand", style={'text-align': 'center'}),
+    dcc.Dropdown(df.schools_province.unique(), 'พัทลุง', id='dropdown-selection'),
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='bar_chart'),
     dcc.Graph(id='map')
 ])
 
-@callback(
-    Output('map', 'figure'),
-    Input('dropdown-selection', 'value')
+@app.callback(
+    [Output('output_container', 'children'),
+     Output('bar_chart', 'figure'),
+     Output('map', 'figure')],
+    [Input('dropdown-selection', 'value')]
 )
-def update_data(value):
-    dff = df[df.schools_province == value]
-    
-    fig = px.choropleth_mapbox(dff, geojson=geojson, 
-                               color="totalstd",
-                               locations="schools_province", featureidkey="properties.province",
-                               center={"lat": 13.736717, "lon": 100.523186},
-                               mapbox_style="carto-positron", zoom=6)  
-    
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    return fig
+def update_graphs(selected_province):
+    container = f"The province chosen by user was: {selected_province}"
 
+    dff = df[df["schools_province"] == selected_province]
+
+    # Bar chart
+    bar_fig = px.bar(
+        dff,
+        x=['Total', 'Male', 'Female'],
+        y=[dff['totalstd'].iloc[0], dff['totalmale'].iloc[0], dff['totalfemale'].iloc[0]],
+        title=f'The quantity of graduates in  {selected_province}',
+        labels={'x': 'Category', 'y': 'The quantity'}
+    )
+
+    # Map
+    map_fig = px.choropleth_mapbox(
+        df,geojson=geojson,
+        locations='schools_province',
+        color='totalstd',
+        featureidkey="properties.name",
+        center={"lat": 7.5435, "lon": 	99.9972}, 
+        mapbox_style="carto-positron",
+        zoom=5,
+        opacity=0.5,
+        labels={'totalstd':'Total Students'}
+    )
+    map_fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    map_fig.add_traces(px.choropleth_mapbox(
+        df[df['schools_province'] == selected_province],
+        geojson=geojson,
+        locations='schools_province',
+        color='totalstd',
+        featureidkey="properties.name",
+        center={"lat": 7.5435, "lon": 	99.9972},
+        mapbox_style="carto-positron",
+        zoom=4, opacity=1,
+        labels={'totalstd':'Total Students'}
+    ).data)
+
+    return container, map_fig, bar_fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
